7613379bf5d593c7c8768c3045b34063106196fa
Aidan Williams <aidanwilliams01@gmail.com>
2023-07-05 15:02:38 -0700

    add constructor test

---
+++ __tests__/rectangle.test.js
@@ -0,0 +1,17 @@
+import inputDate from '../src/weekday.js';
+
+describe('inputDate', () => {
+  let date;
+  let inputDate;
+
+  beforeEach(() => {
+    date = new Date('7/5/2023');
+    inputDate = new inputDate(date, date.getDay(), '')
+  });
+
+  test('should correctly create an inputDate object with three properties', () => {
+    expect(inputDate.date).toEqual('Wed Jul 05 2023 00:00:00 GMT-0700 (Pacific Daylight Time)');
+    expect(inputDate.dayOfWeek).toEqual(3);
+    expect(inputDate.dayOfWeekName).toEqual('');
+  });
+})

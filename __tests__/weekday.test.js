2b60971e98febc697a96ecfc6062ee2dc52a6e2a
Aidan Williams <aidanwilliams01@gmail.com>
2023-07-05 15:34:39 -0700

    edit testing and add constructor

--- __tests__/weekday.test.js
+++ __tests__/weekday.test.js
@@ -5,13 +5,13 @@ describe('inputDate', () => {
   let inputDate;
 
   beforeEach(() => {
-    date = new Date('7/5/2023');
+    date = new Date();
     inputDate = new InputDate(date, date.getDay(), '')
   });
 
   test('should correctly create an inputDate object with three properties', () => {
-    expect(inputDate.date).toEqual('Wed Jul 05 2023 00:00:00 GMT-0700 (Pacific Daylight Time)');
-    expect(inputDate.dayOfWeek).toEqual(3);
+    expect(inputDate.date).toEqual(date);
+    expect(inputDate.dayOfWeek).toEqual(date.getDay());
     expect(inputDate.dayOfWeekName).toEqual('');
   });
 })
--- src/weekday.js
+++ src/weekday.js
@@ -1,3 +1,7 @@
 export default class InputDate {
-  
+  constructor(date, dayOfWeek, dayOfWeekName) {
+    this.date = date;
+    this.dayOfWeek = dayOfWeek;
+    this.dayOfWeekName = dayOfWeekName;
+  }
 }

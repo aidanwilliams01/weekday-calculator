f08bcea0e9ea381a28ac318ff2171882216edbec
Aidan Williams <aidanwilliams01@gmail.com>
2023-07-05 15:44:14 -0700

    add test

--- __tests__/weekday.test.js
+++ __tests__/weekday.test.js
@@ -1,17 +1,17 @@
 import InputDate from "../src/weekday";
 
 describe('inputDate', () => {
-  let date;
-  let inputDate;
-
-  beforeEach(() => {
-    date = new Date();
-    inputDate = new InputDate(date, date.getDay(), '')
-  });
 
   test('should correctly create an inputDate object with three properties', () => {
+    const date = new Date();
+    const inputDate = new InputDate(date, date.getDay(), '')
     expect(inputDate.date).toEqual(date);
     expect(inputDate.dayOfWeek).toEqual(date.getDay());
     expect(inputDate.dayOfWeekName).toEqual('');
+  });
+
+  test('should correctly update dayOfWeekName with the weekday name', () => {
+    const inputDate = new InputDate('7/5/2023', date.getDay(), '');
+    expect(inputDate.dayOfWeekName).toEqual('Wednesday');
   });
 })

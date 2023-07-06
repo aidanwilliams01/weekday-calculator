70c663a88bd22438aa33b2920e944e445cf62440
Aidan Williams <aidanwilliams01@gmail.com>
2023-07-06 16:10:16 -0700

    add CSS styling

--- src/index.js
+++ src/index.js
@@ -7,11 +7,14 @@ function handleForm() {
   event.preventDefault();
   const date = new Date(document.querySelector("input").value);
   if (date.toString() === 'Invalid Date') {
+    document.querySelector("p").setAttribute('class', 'error');
     document.querySelector("p").innerText = 'Invalid date.';
   }
   else {
+
     const dayOfWeek = date.getDay();
     const inputDate = new InputDate(date, dayOfWeek);
+    document.querySelector("p").removeAttribute('class');
     document.querySelector("p").innerText = inputDate.getDayName();
   }
 }
